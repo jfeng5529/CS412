82ce746ee91bdd0dec86ddb9d5e701f1bfc73991
jfeng5529 <jfeng5529@bths.edu>
2020-10-28 23:20:26 -0400

    finialized api calls

--- routes/ps4.js
+++ routes/ps4.js
@@ -3,13 +3,15 @@ let router = express.Router();
 const request = require('request');
 let config = require('../config/config.js');
 
-const getData = (region) => {
+const getData = (postalCode) => {
+  console.log(config);
   return new Promise((resolve, reject) => {
-    request(config.url, (err, res, body) => {
+    console.log(config.url + "us." + postalCode + "?api_id=" + config.apiId + "&app_key=" + config.apiKey);
+    request(config.url+"us."+postalCode+"?app_id="+config.apiId+"&app_key="+config.apiKey, (err, res, body) => {
       if(err){
         reject(new Error(err));
       }else{
-        let info = JSON.parse(body).results[0];
+        let info = JSON.parse(body);
         resolve(info)
       }
 
@@ -17,17 +19,13 @@ const getData = (region) => {
   });
 
 };
-/* GET */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 /* POST */
 // router.post('/submit-form', function (req, res, next) {
-router.get('/submit-form', function (req, res, next) {
-  const region = req.body.region;
-  // getData(region).then((cases) => res.render('postResponse', { 'response': cases}));
-  getData(region).then((cases) =>res.send(cases));
+router.post('/', function (req, res, next) {
+  const postalCode = req.body.postalCode;
+  getData(postalCode).then((cases) => res.render('postResponse', { 'response': cases, 'postalCode': postalCode}));
+  // getData(postalCode).then((cases) =>res.send(cases));
 });
   
 
